27497cf5dc6314ae69ffa5c1d9cab62a6e0f12ae
xoadogithub <42627661+xoadogithub@users.noreply.github.com>
2018-08-24 11:54:49 +0800

    修改BUG   原因：查询资料显示已经删除的资料

--- xoado-FilingCabinet/src/main/java/com/xoado/FilingCabinet/service/impl/Archives.java
+++ xoado-FilingCabinet/src/main/java/com/xoado/FilingCabinet/service/impl/Archives.java
@@ -70,7 +70,7 @@ public class Archives implements IArchives{
 		String archives_Id = MD5.MD5Encode(UUID.randomUUID().toString());
 		TblArchives archives = TblArchives.addArchives(archives_Id, archive_Name, owner_Orgid, owner_Orgname, token.getUserId(), token.getUserName(), comments, new Date(), token.getUserId(), token.getUserName());
 		TblCurrentArchives tblCurrentArchives = TblCurrentArchives.addCurrentArchives(token.getUserId(), archives_Id);
-		
+//		
 		archivesMapper.insert(archives);
 		tblCurrentArchivesMapper.insert(tblCurrentArchives);
 
